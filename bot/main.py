13382d3f4a56057da222dd468dd1dd0978f26517
Tom DiCato <dangywing@gmail.com>
2021-07-01 07:58:02 -0400

    renamed to floorbot

--- bot/main.py
+++ bot/main.py
@@ -16,7 +16,7 @@ token = os.getenv("FLOOR_BOT_TOKEN")
 
 
 @slash.slash(
-    name="floorfinder",
+    name="floorbot",
     description="Finds an OpenSea floor per project",
     # guild_ids=[
     #    # Dangywing Test Server
