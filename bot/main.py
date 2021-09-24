488a105615a4ca407be1978a3bc6ca4a16e6acdf
tomdicato <tomdicato@gmail.com>
2021-09-24 13:59:07 -0400

    Removed typing extensions

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,3 @@
-from typing_extensions import Required
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_choice, create_option
